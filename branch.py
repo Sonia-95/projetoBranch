6622be5c832f833a727ef149b4d89fb040f41299
Sônia Oliveira <soniagomes1995@gmail.com>
2022-12-14 01:58:22 -0300

    commit de conteudo
    Estou fazendo um commit da branch conteudo

--- branch.py
+++ branch.py
@@ -1,11 +1,16 @@
-lista = []
-for recebe in range(2):
-    nome = input('Nome: ')
-    idade = int(input('idade: '))
-    profissao = input('prof: ')
-    lista.append(nome)
-    lista.append(idade)
-    lista.append(profissao)
-print(lista)
 
-print('ESSE PRINT FOI REALIZADO DO PERFIL DO GITHUB, OU SEJA, MODIFIQUEI DO SERVIDOR REMOTO')
+print('Estou modificando a minha Branch conteudo')
+
+class Conteudo:
+    def comandoLista(self):
+
+        lista = []
+        for recebe in range(2):
+            nome = input('Nome: ')
+            idade = int(input('idade: '))
+            profissao = input('prof: ')
+            lista.append(nome)
+            lista.append(idade)
+            lista.append(profissao)
+        print(lista)
+
